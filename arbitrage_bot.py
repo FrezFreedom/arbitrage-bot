391d7c5af051158d8c9b5d4d39ef14e4a6becb8d
frezfreedom <frez1374@gmail.com>
2022-02-24 08:39:07 -0800

    Add nobitex info

--- arbitrage_bot.py
+++ arbitrage_bot.py
@@ -21,3 +21,25 @@ wallex_bid_volume = raw_wallex_data['bidVolume']
 wallex_ask_volume = raw_wallex_data['askVolume']
 
 print(wallex_ask_price, wallex_bid_price, wallex_ask_volume, wallex_bid_volume)
+
+
+####################################################
+
+request_to_nobitex = "https://api.nobitex.ir/v2/orderbook/USDTIRT"
+    
+while True:
+    try:
+        response_from_nobitex = requests.get(request_to_nobitex)
+        nobitex_data = response_from_nobitex.json()
+        if nobitex_data['status'] == "ok":
+            break
+    except Exception as error:
+        time.sleep(5)
+
+
+nobitex_bid_price = nobitex_data['asks'][0][0]
+nobitex_ask_price = nobitex_data['bids'][0][0]
+nobitex_bid_volume = nobitex_data['asks'][0][1]
+nobitex_ask_volume = nobitex_data['bids'][0][1]
+
+print(nobitex_ask_price, nobitex_bid_price, nobitex_ask_volume, nobitex_bid_volume)
