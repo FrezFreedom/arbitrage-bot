167b0746e66e7a5230d39a9246aeb164595ec73d
frezfreedom <frez1374@gmail.com>
2022-02-25 08:22:19 -0800

    Update wallex request

--- arbitrage_bot.py
+++ arbitrage_bot.py
@@ -1,8 +1,9 @@
+from itertools import cycle
 from api_keys import *
 import time
 import requests
 
-request_to_wallex = "https://api.wallex.ir/v1/markets"
+request_to_wallex = "https://api.wallex.ir/v1/depth?symbol=USDTTMN"
     
 while True:
     try:
@@ -13,14 +14,14 @@ while True:
 
 wallex_data = response_from_wallex.json()
 
-raw_wallex_data = wallex_data['result']['symbols']['USDTTMN']['stats']
+raw_wallex_data = wallex_data['result']
 
-wallex_bid_price = raw_wallex_data['bidPrice']
-wallex_ask_price = raw_wallex_data['askPrice']
-wallex_bid_volume = raw_wallex_data['bidVolume']
-wallex_ask_volume = raw_wallex_data['askVolume']
+wallex_bid_price = float(raw_wallex_data['bid'][0]['price'])
+wallex_ask_price = float(raw_wallex_data['ask'][0]['price'])
+wallex_bid_volume = float(raw_wallex_data['bid'][0]['quantity'])
+wallex_ask_volume = float(raw_wallex_data['ask'][0]['quantity'])
 
-print(wallex_ask_price, wallex_bid_price, wallex_ask_volume, wallex_bid_volume)
+print("wallex ", wallex_ask_price, wallex_bid_price, wallex_ask_volume, wallex_bid_volume)
 
 
 ####################################################
@@ -37,12 +38,12 @@ while True:
         time.sleep(5)
 
 
-nobitex_bid_price = nobitex_data['asks'][0][0]
-nobitex_ask_price = nobitex_data['bids'][0][0]
-nobitex_bid_volume = nobitex_data['asks'][0][1]
-nobitex_ask_volume = nobitex_data['bids'][0][1]
+nobitex_bid_price = float(nobitex_data['asks'][0][0]) / 10.00
+nobitex_ask_price = float(nobitex_data['bids'][0][0]) / 10.00
+nobitex_bid_volume = float(nobitex_data['asks'][0][1])
+nobitex_ask_volume = float(nobitex_data['bids'][0][1])
 
-print(nobitex_ask_price, nobitex_bid_price, nobitex_ask_volume, nobitex_bid_volume)
+print("nobitex ", nobitex_ask_price, nobitex_bid_price, nobitex_ask_volume, nobitex_bid_volume)
 
 #################################################
 
@@ -59,12 +60,12 @@ exir_data = response_from_exir.json()
 
 raw_exir_data = exir_data['usdt-irt']
 
-exir_bid_price = raw_exir_data['bids'][0][0]
-exir_ask_price = raw_exir_data['asks'][0][0]
-exir_bid_volume = raw_exir_data['bids'][0][1]
-exir_ask_volume = raw_exir_data['asks'][0][1]
+exir_bid_price = float(raw_exir_data['bids'][0][0])
+exir_ask_price = float(raw_exir_data['asks'][0][0])
+exir_bid_volume = float(raw_exir_data['bids'][0][1])
+exir_ask_volume = float(raw_exir_data['asks'][0][1])
 
-print(exir_ask_price, exir_bid_price, exir_ask_volume, exir_bid_volume)
+print("exir ", exir_ask_price, exir_bid_price, exir_ask_volume, exir_bid_volume)
 
 #################################################
 
@@ -81,11 +82,35 @@ arzpaya_data = response_from_arzpaya.json()
 
 raw_arzpaya_data = arzpaya_data['USDTIR']
 
-arzpaya_bid_price = raw_arzpaya_data['Buys'][0]['Price']
-arzpaya_ask_price = raw_arzpaya_data['Sells'][0]['Price']
-arzpaya_bid_volume = raw_arzpaya_data['Buys'][0]['Volume']
-arzpaya_ask_volume = raw_arzpaya_data['Sells'][0]['Volume']
+arzpaya_bid_price = float(raw_arzpaya_data['Buys'][0]['Price'])
+arzpaya_ask_price = float(raw_arzpaya_data['Sells'][0]['Price'])
+arzpaya_bid_volume = float(raw_arzpaya_data['Buys'][0]['Volume'])
+arzpaya_ask_volume = float(raw_arzpaya_data['Sells'][0]['Volume'])
 
-print(arzpaya_ask_price, arzpaya_bid_price, arzpaya_ask_volume, arzpaya_bid_volume)
+print("arzpaya ", arzpaya_ask_price, arzpaya_bid_price, arzpaya_ask_volume, arzpaya_bid_volume)
+
+#################################################
+
+request_to_okex = "https://ok-ex.io/server/api/order/order-books"
+    
+while True:
+    try:
+        response_from_okex = requests.get(request_to_okex)
+        break
+    except Exception as error:
+        time.sleep(5)
+
+okex_data = response_from_okex.json()
+
+for crypto in okex_data:
+    if crypto['market']['symbol']=="USDTIRT":
+        okex_bid_price = float(crypto['bids'][len(crypto['bids'])-1]['p'])
+        okex_ask_price = float(crypto['asks'][0]['p'])
+        okex_bid_volume = float(crypto['bids'][len(crypto['bids'])-1]['qt'])
+        okex_ask_volume = float(crypto['asks'][0]['qt'])
+        break
+
+
+print("okex ", okex_ask_price, okex_bid_price, okex_ask_volume, okex_bid_volume)
 
 #################################################
